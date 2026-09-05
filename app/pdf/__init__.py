d437cf21b4dda141a5e801fc8163db3e8d250de5
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-09-05 11:12:33 -0700

    pdf: export canonical judgment metadata helpers

--- app/pdf/__init__.py
+++ app/pdf/__init__.py
@@ -1,10 +1,30 @@
 from .acquisition import PdfAcquisitionService, PdfArtifact
-from .compilation import extract_page_range, locate_case_page_range, verify_case_number_in_pdf
+from .compilation import (
+    JudgmentMetadata,
+    extract_judgment_metadata,
+    extract_judgment_metadata_from_pdf,
+    extract_page_range,
+    labeled_case_numbers_in_pdf,
+    locate_case_page_range,
+    refine_case_page_range,
+    verify_case_number_in_pdf,
+)
 from .errors import PdfAcquisitionError, PdfSecurityError, PdfValidationError
 from .text import extract_pdf_text
 
 __all__ = [
-    "PdfAcquisitionService", "PdfArtifact", "PdfAcquisitionError", "PdfSecurityError",
-    "PdfValidationError", "extract_page_range", "locate_case_page_range",
-    "verify_case_number_in_pdf", "extract_pdf_text",
+    "PdfAcquisitionService",
+    "PdfArtifact",
+    "PdfAcquisitionError",
+    "PdfSecurityError",
+    "PdfValidationError",
+    "JudgmentMetadata",
+    "extract_judgment_metadata",
+    "extract_judgment_metadata_from_pdf",
+    "extract_page_range",
+    "labeled_case_numbers_in_pdf",
+    "locate_case_page_range",
+    "refine_case_page_range",
+    "verify_case_number_in_pdf",
+    "extract_pdf_text",
 ]

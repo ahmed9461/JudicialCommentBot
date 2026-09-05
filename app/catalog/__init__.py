7a82bfbe53d1d416d591b05ec9be1d14c211c84c
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-09-05 15:17:32 -0700

    catalog: expose active parser generation

--- app/catalog/__init__.py
+++ app/catalog/__init__.py
@@ -1,10 +1,11 @@
 from .errors import CatalogNotReadyError
-from .indexer import IndexReport, OfficialCatalogIndexer
+from .indexer import CATALOG_PARSER_VERSION, IndexReport, OfficialCatalogIndexer
 from .manifest import CatalogManifest, CatalogManifestLoader, CatalogSourceSpec
 from .provider import CatalogFirstResearchProvider, CatalogResearchProvider, SubjectSourceMap
 from .store import CatalogStore
 
 __all__ = [
+    "CATALOG_PARSER_VERSION",
     "CatalogFirstResearchProvider",
     "CatalogManifest",
     "CatalogManifestLoader",

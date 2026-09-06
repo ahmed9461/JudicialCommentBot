e7af8939f3937ec8c2ad0e6f73402b4ffd1766a9
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-09-06 13:44:02 -0700

    pdf: export primary-boundary validation helpers

--- app/pdf/__init__.py
+++ app/pdf/__init__.py
@@ -6,10 +6,13 @@ from .compilation import (
     extract_page_range,
     labeled_case_numbers_in_pdf,
     locate_case_page_range,
+    primary_case_numbers_in_pdf,
     refine_case_page_range,
+    validate_extracted_judgment_pdf,
     verify_case_number_in_pdf,
 )
 from .errors import PdfAcquisitionError, PdfSecurityError, PdfValidationError
+from .headers import JudicialHeader, primary_case_number, primary_judicial_header
 from .text import extract_pdf_text
 
 __all__ = [
@@ -19,12 +22,17 @@ __all__ = [
     "PdfSecurityError",
     "PdfValidationError",
     "JudgmentMetadata",
+    "JudicialHeader",
     "extract_judgment_metadata",
     "extract_judgment_metadata_from_pdf",
     "extract_page_range",
     "labeled_case_numbers_in_pdf",
     "locate_case_page_range",
+    "primary_case_number",
+    "primary_case_numbers_in_pdf",
+    "primary_judicial_header",
     "refine_case_page_range",
+    "validate_extracted_judgment_pdf",
     "verify_case_number_in_pdf",
     "extract_pdf_text",
 ]

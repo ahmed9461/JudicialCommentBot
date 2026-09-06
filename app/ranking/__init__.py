259dbdb56c6523d89311d4ef3b0bd77e8ef984fb
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-09-06 13:45:11 -0700

    ranking: export subject relevance assessment

--- app/ranking/__init__.py
+++ app/ranking/__init__.py
@@ -1,4 +1,12 @@
+from .relevance import SubjectRelevanceAssessment, assess_subject_relevance
 from .scoring import ScoreResult, ScoringPolicy
 from .selection import SelectionDecision, select_candidates
 
-__all__ = ["ScoreResult", "ScoringPolicy", "SelectionDecision", "select_candidates"]
+__all__ = [
+    "ScoreResult",
+    "ScoringPolicy",
+    "SelectionDecision",
+    "SubjectRelevanceAssessment",
+    "assess_subject_relevance",
+    "select_candidates",
+]
